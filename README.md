9be222212438550ffb4009b4f8b51bd22adc331d
ricardo <rcastro@ite.edu.mx>
2024-10-19 10:51:33 -0700

    Trabajo para evaluación

---
+++ README.md
@@ -0,0 +1,4 @@
+# Evaluación
+
+Deberá entregar una captura de pantalla del sitio funcionando, cuyo dominio sea forzosamente "al" + "número de control". test; por ejemplo:
+http://al12345678.test
---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormularioComponent } from './formulario/formulario.component';
+import {MatInputModule} from "@angular/material/input";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormularioComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
